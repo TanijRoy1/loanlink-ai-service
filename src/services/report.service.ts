54cc9c0a52c7f1fdc788188b74b7a85ea98d42a2
TanijRoy1 <tanishroy480@gmail.com>
2026-06-18 11:13:04 +0600

    feat: create report persistence service

---
+++ src/services/report.service.ts
@@ -0,0 +1,23 @@
+import crypto from "crypto";
+import { prisma } from "../config/db";
+import { LoanSummaryInput, LoanSummaryOutput } from "../types/report.types";
+
+export const saveReport = async (
+  loanData: LoanSummaryInput,
+  aiReport: LoanSummaryOutput,
+) => {
+  return prisma.report.create({
+    data: {
+      loanId: crypto.randomUUID(),
+      userId: crypto.randomUUID(),
+
+      summary: aiReport.summary,
+
+      repaymentAnalysis: aiReport.repaymentAnalysis,
+
+      riskAnalysis: aiReport.riskAnalysis,
+
+      status: "completed",
+    },
+  });
+};
