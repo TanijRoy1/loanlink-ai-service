431a6d48243fdcd75d27c71de85b0b1d9909bde1
TanijRoy1 <tanishroy480@gmail.com>
2026-06-22 17:01:10 +0600

    feat: create PDF service skeleton

--- src/types/report.types.ts
+++ src/types/report.types.ts
@@ -12,3 +12,16 @@ export interface LoanSummaryOutput {
   riskAnalysis: string;
   recommendations: string[];
 }
+
+export interface PDFReportData {
+  applicantName: string;
+  loanAmount: number;
+  duration: number;
+  purpose: string;
+
+  summary: string;
+  repaymentAnalysis: string;
+  riskAnalysis: string;
+
+  recommendations: string[];
+}
